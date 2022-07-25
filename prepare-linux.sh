e3a0f3753a1c5cae954fcb24edfb6e6d4e750f82
Kees Cook <keescook@chromium.org>
2022-07-25 10:04:20 -0700

    prepare-linux: Use in-tree .config hunks
    Instead of using scripts/config, use the newly added .config files for
    the workshop.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- prepare-linux.sh
+++ prepare-linux.sh
@@ -9,14 +9,7 @@ mkdir -p $BUILD
 
 MAKE="make LLVM=1 O=$BUILD -j$(getconf _NPROCESSORS_ONLN)"
 
-$MAKE defconfig kvm_guest.config
-./scripts/config --file $BUILD/.config \
-	--set-val PANIC_ON_OOPS_VALUE 1 \
-	--set-val PANIC_TIMEOUT -1 \
-	-e SLAB_FREELIST_RANDOM \
-	-e SLAB_FREELIST_HARDENED \
-	-e HARDENED_USERCOPY \
-	-e FORTIFY_SOURCE \
-	-e PVPANIC \
-	-e WORKSHOP \
-	#
+CONFIGS="defconfig kvm_guest.config workshop.config"
+#CONFIGS="$CONFIGS ubsan_bounds.config"
+
+$MAKE $CONFIGS
