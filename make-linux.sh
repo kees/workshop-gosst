fdd9710ae74cb548dfa95ff30133ee0a7f929d71
Kees Cook <keescook@chromium.org>
2022-07-25 11:49:30 -0700

    make-linux: Use environment for build target
    Don't open-code the path.
    
    Signed-off-by: Kees Cook <keescook@chromium.org>

--- make-linux.sh
+++ make-linux.sh
@@ -10,6 +10,6 @@ LLVM=1
 if [ -x "$CLANG_DIR"/clang ]; then
 	LLVM=$CLANG_DIR/
 fi
-MAKE="make LLVM=$LLVM O=../builds/linux -j$(getconf _NPROCESSORS_ONLN)"
+MAKE="make LLVM=$LLVM O=$BUILD -j$(getconf _NPROCESSORS_ONLN)"
 cd $SRC
 $MAKE -s olddefconfig bzImage
